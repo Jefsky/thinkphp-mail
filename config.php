99f3c1efede6093b70fe58c3d3cd46829a0357e4
Jefsky <jefsky@live.com>
2017-07-12 14:10:34 +0800

    新建 config.php

---
+++ config.php
@@ -0,0 +1,10 @@
+//邮箱配置
+	'MAIL_HOST' =>'smtp.qq.com',//smtp服务器的名称
+	'SMTP_PORT'   => '465', //SMTP服务器端口
+	'MAIL_SMTPAUTH' =>TRUE, //启用smtp认证
+	'MAIL_USERNAME' =>'***@qq.com',//你的邮箱名
+	'MAIL_FROM' =>'***@qq.com',//发件人地址
+	'MAIL_FROMNAME'=>'***',//发件人姓名
+	'MAIL_PASSWORD' =>'***',//邮箱密码
+	'MAIL_CHARSET' =>'utf-8',//设置邮件编码
+	'MAIL_ISHTML' =>TRUE, // 是否HTML格式邮件
